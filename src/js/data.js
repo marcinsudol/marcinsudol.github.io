0011cdc67bc5b242c94b7bcdc2479c188e45cb27
marcinsudol <marcin.sudol@outlook.com>
2020-12-17 00:14:03 +0100

    added data.js

---
+++ src/js/data.js
@@ -0,0 +1,91 @@
+const projects = [
+  {
+    title: "Projects done for freeCodeCamp RWD certificate",
+    projects: [
+      {
+        name: "tribute page",
+        page: "https://marcinsudol.github.io/tribute-page/",
+        repo: "https://github.com/marcinsudol/tribute-page",
+        tech: ["css", "html"],
+      },
+      {
+        name: "survey form",
+        page: "https://marcinsudol.github.io/survey-form/",
+        repo: "https://github.com/marcinsudol/survey-form",
+        tech: ["css", "html"],
+      },
+      {
+        name: "landing page",
+        page: "https://marcinsudol.github.io/landing-page/",
+        repo: "https://github.com/marcinsudol/landing-page",
+        tech: ["css", "html"],
+      },
+      {
+        name: "documentation page",
+        page: "https://marcinsudol.github.io/documentation-page/",
+        repo: "https://github.com/marcinsudol/documentation-page",
+        tech: ["css", "html"],
+      },
+      {
+        name: "portfolio page",
+        page: "https://marcinsudol.github.io/portfolio-page/",
+        repo: "https://github.com/marcinsudol/portfolio-page",
+        tech: ["css", "html"],
+      },
+    ],
+  },
+  {
+    title: "Projects done for freeCodeCamp frameworks certificate",
+    projects: [
+      {
+        name: "random quote generator",
+        page: "https://marcinsudol.github.io/random-quote/",
+        repo: "https://github.com/marcinsudol/random-quote",
+        tech: ["jquery", "bootstrap", "html"],
+      },
+      {
+        name: "markdown previewer",
+        page: "https://marcinsudol.github.io/markdown-previewer/",
+        repo: "https://github.com/marcinsudol/markdown-previewer",
+        tech: ["jquery", "scss", "html"],
+      },
+      {
+        name: "drum machine",
+        page: "https://marcinsudol.github.io/drum-machine/",
+        repo: "https://github.com/marcinsudol/drum-machine",
+        tech: ["react", "scss"],
+      },
+      {
+        name: "calculator",
+        page: "https://marcinsudol.github.io/calculator/",
+        repo: "https://github.com/marcinsudol/calculator",
+        tech: ["react", "scss"],
+      },
+      {
+        name: "25+5 clock",
+        page: "https://marcinsudol.github.io/25-5-clock/",
+        repo: "https://github.com/marcinsudol/25-5-clock",
+        tech: ["react", "scss"],
+      },
+    ],
+  },
+  {
+    title: "My projects",
+    projects: [
+      {
+        name: "to do list",
+        page: "https://marcinsudol.github.io/to-do-list/",
+        repo: "https://github.com/marcinsudol/to-do-list",
+        tech: ["react", "bootstrap"],
+      },
+      {
+        name: "recipe book",
+        page: "https://marcinsudol.github.io/recipe-book/",
+        repo: "https://github.com/marcinsudol/recipe-book",
+        tech: ["vanilla js", "scss", "html"],
+      },
+    ],
+  },
+];
+
+export default projects;
